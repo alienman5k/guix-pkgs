b0c651b1c41d968aebf8c4dc089fddee0b5b9622
Ignacio Marmolejo <ignacio.marmolejo@gmail.com>
2025-09-01 22:26:39 -0700

    Update neovim plugins

--- am5k/packages/neovim-plugins.scm
+++ am5k/packages/neovim-plugins.scm
@@ -7,6 +7,30 @@
   #:use-module (gnu packages vim)
   #:use-module (guix gexp))
 
+(define-public blink-cmp
+  (package
+    (name "neovim-blink.cmp")
+    (version "1.6.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/Saghen/blink.cmp")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0af09kssb9n2dxmf2i79kgdnln6f42nba5gg391y4gqqly05hx10"))))
+    (build-system vim-build-system)
+    (arguments
+     (list
+      #:plugin-name "blink.cmp"))
+    (home-page "https://github.com/Saghen/blink.cmp")
+    (synopsis
+     "Completion plugin with support for LSPs, cmdline, signature help and snippets")
+    (description
+     "blink.cmp is a completion plugin with support for LSPs, cmdline, signature help and snippets. It uses an optional custom fuzzy matcher for typo resistance. It provides extensibility via pluggable sources (LSP, buffer, snippets, etc), component based rendering and scripting for the configuration.")
+    (license license:expat)))
+
 (define-public mini
   (package
     (name "neovim-mini")
@@ -19,7 +43,7 @@
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "12027xb9907zk145hsx6qniq1cjm8bm5405njq4cs9vx992pafsh"))))
+        (base32 "1gb5l58vmyiknk7nhyn2r3d02yd4jsd5kpv3q59ja7pzdi0dyng4"))))
     (build-system vim-build-system)
     (arguments
      (list
@@ -97,6 +121,28 @@
      "Neovim pluing that allows Navigation and manipulation of the file system.")
     (license license:expat)))
 
+(define-public mini-icons
+  (package
+    (name "neovim-mini.icons")
+    (version "0.16.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/nvim-mini/mini.icons")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "110bglbbyafjym4md2slgccyjhf90bgg8h9h2ipya6cfqfs4pizy"))))
+    (build-system vim-build-system)
+    (arguments
+     (list
+      #:plugin-name "mini.icons"))
+    (home-page "https://github.com/nvim-mini/mini.icons")
+    (synopsis "Icon provider.")
+    (description "Icon provider.")
+    (license license:expat)))
+
 (define-public mini-indentscope
   (package
     (name "neovim-mini.indentscope")
@@ -140,6 +186,45 @@
     (synopsis "Navigate and manipulate file system")
     (description
      "Neovim pluing that allows Navigation and manipulation of the file system.")
+    (license license:expat)))
+
+(define-public mini-pick-latest
+  (package
+    (inherit mini-pick)
+    (name "neovim-mini.pick")
+    (version "latest")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/nvim-mini/mini.pick")
+             (commit "be6490ae9d7038b9f5185d95a8060054f9b23666")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1yhwb8c44n7bzcqa7vwrh5fqxc6qgx1bxqph43f2sajsgnfd1vhp"))))))
+
+(define-public mini-statusline
+  (package
+    (name "neovim-mini.statusline")
+    (version "0.16.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/nvim-mini/mini.statusline")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1hhd4fln3m04d9v5pwa3mb1n4nifsilrxp8hs14njcgk2rxv6qar"))))
+    (build-system vim-build-system)
+    (arguments
+     (list
+      #:plugin-name "mini.statusline"))
+    (home-page "https://github.com/nvim-mini/mini.statusline")
+    (synopsis
+     "Minimal and fast statusline module with opinionated default look.")
+    (description
+     "Minimal and fast statusline module with opinionated default look.")
     (license license:expat)))
 
 (define-public solarized
