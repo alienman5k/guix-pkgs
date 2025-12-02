7e3fc2b487569ab91e67dffb5aaf980352565e4f
Ignacio Marmolejo <ignacio.marmolejo@gmail.com>
2025-12-02 15:16:15 -0800

    Format aliensevka package

--- am5k/packages/fonts.scm
+++ am5k/packages/fonts.scm
@@ -1,22 +1,22 @@
 (define-module (am5k packages fonts)
-   #:use-module (guix packages)
-   #:use-module (guix download)
-   #:use-module (guix build-system font)
-   #:use-module ((guix licenses) #:prefix license:))
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system font))
 
 (define-public font-aliensevka
   (package
     (name "font-aliensevka")
     (version "1.1")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append 
-		     "https://github.com/alienman5k/aliensevka/releases/download/v" 
-		     version 
-		     "/Aliensevka.zip"))
-              (sha256
-               (base32
-                "056ag5g1jk0mshifims3zpmp13sy6r4yhnpylmxnhrvbgn00bjim"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://github.com/alienman5k/aliensevka/releases/download/v"
+             version "/Aliensevka.zip"))
+       (sha256
+        (base32 "056ag5g1jk0mshifims3zpmp13sy6r4yhnpylmxnhrvbgn00bjim"))))
     (build-system font-build-system)
     (home-page "https://github.com/alienman5k/aliensevka")
     (synopsis "Aliensevka Font based on Iosevka")
